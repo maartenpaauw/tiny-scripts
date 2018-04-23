a50e83c329f73106ac00927883512eee0e665602
Maarten Paauw <maartenpaauw@gmail.com>
2018-04-23 10:13:00 +0200

    chore(ffpmeg): serie helper

---
+++ ffmpeg/ffmpeg.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Ask for download URL.
+echo "Download URL:";
+read url;
+
+# Ask for Serie name.
+echo "Serie";
+read serie;
+
+# Ask for season number.
+echo "Seizoen:";
+read season;
+
+# Ask for episode number.
+echo "Aflevering:"; 
+read episode;
+
+# Download episode to specific folder.
+ffmpeg -i ${url} "/Volumes/Series/${serie}/S${season}/E${episode}/${serie} - S${season}E${episode}.mp4"
